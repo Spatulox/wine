e6191d95a2da6c5824d630939f230957ac8110c9
Spatulox <M78stormtrooper@laposte.net>
2026-03-02 14:50:43 +0100

    add unique constraint onto Stock for the shlef and position field

--- app/src/main/java/com/spatulox/wine/data/db/entity/StockEntity.kt
+++ app/src/main/java/com/spatulox/wine/data/db/entity/StockEntity.kt
@@ -2,6 +2,7 @@ package com.spatulox.wine.data.db.entity
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
@@ -13,6 +14,9 @@ import androidx.room.PrimaryKey
             childColumns = ["wineId"],
             onDelete = ForeignKey.CASCADE
         )
+    ],
+    indices = [
+        Index(value = ["shelf", "position"], unique = true)
     ]
 )
 data class StockEntity(
