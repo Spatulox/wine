d2b08ea89b4050a25a023a63dbc4b8dc573951a0
Spatulox <M78stormtrooper@laposte.net>
2026-03-02 14:52:14 +0100

    Update stock insertion with history preparation
    Udpaet the `insert` method to add stock items to the repository.
    The method constructs a history entry of type `ADD` with a provided reason, laying the groundwork for traceability of stock changes.
    Leverages `TransactionProvider` to ensure transactional integrity.

--- app/src/main/java/com/spatulox/wine/data/repository/StockRepositoryImpl.kt
+++ app/src/main/java/com/spatulox/wine/data/repository/StockRepositoryImpl.kt
@@ -1,7 +1,5 @@
 package com.spatulox.wine.data.repository
 
-import androidx.room.withTransaction
-import com.spatulox.wine.data.db.AppDatabase
 import com.spatulox.wine.data.db.TransactionProvider
 import com.spatulox.wine.data.db.dao.HistoryDao
 import com.spatulox.wine.data.db.dao.StockDao
@@ -9,8 +7,11 @@ import com.spatulox.wine.data.mapper.HistoryMapper
 import com.spatulox.wine.domain.repository.StockRepository
 
 import com.spatulox.wine.data.mapper.StockMapper
+import com.spatulox.wine.domain.enum.HistoryType
 import com.spatulox.wine.domain.model.History
 import com.spatulox.wine.domain.model.Stock
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class StockRepositoryImpl(
     private val stockDao: StockDao,
@@ -30,6 +31,21 @@ class StockRepositoryImpl(
     override fun getStockStream(): Flow<List<Stock>> {
         return stockDao.getStockStream().map { entities -> entities.map { StockMapper.toDomain(it) } }
     }
+
+    override suspend fun insert(stock: Stock, reason: String): Long {
+
+        return transactionProvider.run {
+            val stockEntity = StockMapper.toEntity(stock)
+            val history = HistoryMapper.toEntity(
+                History(
+                    wineId = stockEntity.wineId,
+                    type = HistoryType.ADD,
+                    date = System.currentTimeMillis(),
+                    reason = reason
+                )
+            )
+            stockDao.insert(stockEntity)
+        }
     }
 
     override suspend fun withdraw(stock: Stock, reason: String): Long {
