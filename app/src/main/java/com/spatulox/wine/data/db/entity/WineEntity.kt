a67cf7136ce6bacbee9fbdf33ff1af3244bf2c0a
Spatulox <M78stormtrooper@laposte.net>
2026-03-02 13:38:32 +0100

    add unique constraint on wine table

--- app/src/main/java/com/spatulox/wine/data/db/entity/WineEntity.kt
+++ app/src/main/java/com/spatulox/wine/data/db/entity/WineEntity.kt
@@ -1,8 +1,14 @@
 package com.spatulox.wine.data.db.entity
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
-@Entity(tableName = "wine")
+@Entity(
+    tableName = "wine",
+    indices = [
+        Index(value = ["name", "year", "format"], unique = true)
+    ]
+)
 data class WineEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
