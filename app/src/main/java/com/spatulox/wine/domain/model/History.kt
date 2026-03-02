95045d3ff6147a7d6a48d1c909e69ac193709ac6
Spatulox <M78stormtrooper@laposte.net>
2026-03-02 11:34:58 +0100

    init domain

---
+++ app/src/main/java/com/spatulox/wine/domain/model/History.kt
@@ -0,0 +1,6 @@
+package com.spatulox.wine.domain.model
+
+data class History(
+    val id: Int = 0,
+    val date: Long
+)
---
+++ app/src/main/java/com/spatulox/wine/domain/model/Wine.kt
@@ -0,0 +1,7 @@
+package com.spatulox.wine.domain.model
+
+data class Wine(
+    val id: Int = 0,
+    val year: String,
+    val date: Long
+)
