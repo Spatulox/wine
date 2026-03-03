2b75ecd8952180af26709c386b38236a1eac7cb1
Spatulox <M78stormtrooper@laposte.net>
2026-03-03 14:22:59 +0100

    add displayName for wineType

--- app/src/main/java/com/spatulox/wine/domain/enum/WineType.kt
+++ app/src/main/java/com/spatulox/wine/domain/enum/WineType.kt
@@ -1,11 +1,11 @@
 package com.spatulox.wine.domain.enum
 
 // Since wine are french, use the french words
-enum class WineType {
-    ROUGE,
-    BLANC,
-    ROSE,
-    CHAMPAGNE,
-    MOUSSEUX,
-    AUTRE
+enum class WineType(val displayName: String) {
+    ROUGE("Rouge"),
+    BLANC("Blanc"),
+    ROSE("Rosé"),
+    CHAMPAGNE("Champagne"),
+    MOUSSEUX("Mousseaux"),
+    AUTRE("Autre")
 }
