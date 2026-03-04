40875189eafd4be038ffd60f84b8a1fdb1177b0c
Spatulox <M78stormtrooper@laposte.net>
2026-03-04 16:07:16 +0100

    Adds shelf management logic to ViewModel
    Provides a `StateFlow` to observe the list of shelves.
    Implements `insert`, `update`, and `delete` operations for shelf entities.
    Includes error handling for constraint violations during shelf deletion, ensuring robust data management.

--- app/src/main/java/com/spatulox/wine/viewModels/ShelfViewModel.kt
+++ app/src/main/java/com/spatulox/wine/viewModels/ShelfViewModel.kt
@@ -1,7 +1,42 @@
 package com.spatulox.wine.viewModels
 
+import android.database.sqlite.SQLiteConstraintException
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.spatulox.wine.data.repository.ShelfRepositoryImpl
+import com.spatulox.wine.domain.model.Shelf
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 
-class ShelfViewModel(private val shelfRepositoryImpl: ShelfRepositoryImpl): ViewModel() {
+class ShelfViewModel(private val shelfRepository: ShelfRepositoryImpl): ViewModel() {
+
+    val shelves: StateFlow<List<Shelf>> = shelfRepository
+        .getAllShelvesStream()
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000),
+            emptyList()
+        )
+
+    suspend fun insert(shelf: Shelf){
+        shelfRepository.insert(shelf)
+    }
+
+    suspend fun update(shelf: Shelf){
+        shelfRepository.update(shelf)
+    }
+
+    suspend fun delete(shelfId: Int): Boolean{
+        return try {
+            shelfRepository.delete(shelfId)
+            true
+        } catch (e: SQLiteConstraintException) {
+            false
+        }
+    }
+    suspend fun delete(shelf: Shelf): Boolean{
+        return this.delete(shelf.id)
+    }
+
 }
